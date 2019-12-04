881342f0197e7f895d42ed2a264418e67ef782ea
PreranaPandit <preranapandit07@gmail.com>
2019-12-04 10:53:51 +0545

    Fragments first and second added

--- app/src/main/java/com/example/aad_chapter6/MainActivity.java
+++ app/src/main/java/com/example/aad_chapter6/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.aad_chapter6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.aad_chapter6.fragments.FirstFragment;
+import com.example.aad_chapter6.fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button btnFragment;
+
+    //if status is true then load first fragment
+    private Boolean ststus = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFragment = findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(ststus)
+        {
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load second fragment");
+            ststus = false;
+        }
+        else{
+
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load first Fragment");
+            ststus = true;
+        }
     }
 }
